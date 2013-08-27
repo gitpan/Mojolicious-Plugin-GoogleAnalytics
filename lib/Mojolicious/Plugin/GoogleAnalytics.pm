cc61d730adb7a3d44b14d7e6f309b0461082382f
Adam Stokes <adam.stokes@ubuntu.com>
2013-08-27 04:46:49 +0000

    Import of ADAMJS/Mojolicious-Plugin-GoogleAnalytics-1.003 from CPAN.
    gitpan-cpan-distribution: Mojolicious-Plugin-GoogleAnalytics
    gitpan-cpan-version:      1.003
    gitpan-cpan-path:         ADAMJS/Mojolicious-Plugin-GoogleAnalytics-1.003.tar.gz
    gitpan-cpan-author:       ADAMJS
    gitpan-cpan-maturity:     released

--- lib/Mojolicious/Plugin/GoogleAnalytics.pm
+++ lib/Mojolicious/Plugin/GoogleAnalytics.pm
@@ -1,9 +1,8 @@
 package Mojolicious::Plugin::GoogleAnalytics;
 
-use strictures 1;
 use Mojo::Base 'Mojolicious::Plugin';
 
-our $VERSION = '1.002'; # VERSION
+our $VERSION = '1.003';
 
 has 'template' => 'analytics_template';
 
@@ -15,14 +14,18 @@ sub register {
 
     $app->helper(
         analytics_inc => sub {
-            my $self         = shift;
-            my $analytics_id = shift;
+            my $self                  = shift;
+            my $analytics_id          = shift;
+            my $domain_sub            = shift;
+            my $allow_multi_top_level = shift;
 
             die "No analytics ID defined" unless defined $analytics_id;
             $self->render(
-                template     => $self->analytics->template,
-                partial      => 1,
-                analytics_id => $analytics_id,
+                template              => $self->analytics->template,
+                partial               => 1,
+                analytics_id          => $analytics_id,
+                domain_sub            => $domain_sub,
+                allow_multi_top_level => $allow_multi_top_level || undef,
             );
         }
     );
@@ -37,6 +40,12 @@ __DATA__
 %= javascript begin
   var _gaq = _gaq || [];
   _gaq.push(['_setAccount', '<%= $analytics_id %>']);
+  % if ($domain_sub) {
+  _gaq.push(['_setDomainName', <%= $domain_sub %>']);
+  % }
+  % if ($allow_multi_top_level) {
+  _gaq.push(['_setAllowLinker', true]);
+  % }
   _gaq.push(['_trackPageview']);
 
   (function() {
@@ -61,7 +70,7 @@ Mojolicious::Plugin::GoogleAnalytics - Mojolicious Plugin
   plugin 'GoogleAnalytics';
 
   # In your layout template
-  <%= analytics_inc 'UA-32432-1' %>
+  <%= analytics_inc 'UA-32432-1', 'example.com', 1 %>
   </head> <!-- Make sure its just before closing head tag -->
 
 
@@ -79,6 +88,28 @@ L<Mojolicious::Plugin> and implements the following new ones.
   $plugin->register(Mojolicious->new);
 
 Register plugin in L<Mojolicious> application.
+
+=head1 OPTIONS
+
+=head2 Track Subdomains
+
+Put the domain which qualifies any subdomains you wish to track, eg. blog.example.com, apps.example.com will have the second arguement set to 'example.com'
+
+=head2 Multiple top level domains
+
+Default is set to 1 to allow domains such as example.fr, example.cn, and example.com
+
+=head1 AUTHOR
+
+Adam Stokes E<lt>adamjs@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2013- Adam Stokes
+
+=head1 LICENSE
+
+Licensed under the same terms as Perl.
 
 =head1 SEE ALSO
 
